88849023f1f388e95acdccc4eb0b70dce62b8995
Morphen <daemonsea@gmail.com>
2019-04-06 10:23:53 -0500

    Update

--- src/brickBreaker/Tablero.java
+++ src/brickBreaker/Tablero.java
@@ -1,15 +1,22 @@
 package brickBreaker;
 
 import Final.menu;
+import java.applet.AudioClip;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 
 public class Tablero extends JPanel implements Runnable, Constantes {
@@ -18,31 +25,66 @@ public class Tablero extends JPanel implements Runnable, Constantes {
     private final Barra barra;
     private final Bola bola;
     private final Escucha escucha;
-    private final Fuente fuente;
     private final Grilla[][] grilla = new Grilla[7][5];
     private final Thread juego;
     private final ImageIcon img;
+    private Font font;
     static int vidas = 3, puntaje = 0, ladriTotal = 35, nivel = 3;
     static AtomicBoolean pausa;
 
     public Tablero(int width, int height) {
+        //se define el tamaño del panel.
         super.setSize(width, height);
+
+        //permite los escuchas en el panel
         setFocusable(true);
+
+        //se crean los objetos.
         barra = new Barra(BARRA_POS_INICIALX, BARRA_POS_INICIALY, BARRA_WIDTH, BARRA_HEIGHT);
         bola = new Bola(BOLA_POS_INICIALX, BOLA_POS_INICIALY, BOLA_RADIO, BOLA_RADIO);
-        fuente = new Fuente();
         pausa = new AtomicBoolean();
         escucha = new Escucha();
         juego = new Thread(this);
+
+        //se añaden los escuchas.
         addMouseMotionListener(escucha);
         addKeyListener(escucha);
+
+        //se crea la grilla(ladrillos) inicial.
         grilla();
+
+        ReproducirSonido(0);
+        //se empieza el hilo y se pausa.
         juego.start();
         stop();
         pausa.set(true);
+
+        //imagenfondo del panel.
         img = new ImageIcon(getClass().getResource("/Imagenes/background.jpg"));
     }
 
+    //metodo para reproducir los sonidos
+    public void ReproducirSonido(int audio) {
+        try {
+            AudioInputStream audio1 = AudioSystem.getAudioInputStream(new File("src/Sound/golpeBarra.wav").getAbsoluteFile());
+            AudioInputStream audio2 = AudioSystem.getAudioInputStream(new File("src/Sound/golpeGrilla.wav").getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            switch (audio) {
+                case 1:
+                    clip.open(audio1);
+                    clip.start();
+                    break;
+                case 2:
+                    clip.open(audio2);
+                    clip.start();
+                    break;
+            }
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException ex) {
+            System.out.println("Error al reproducir el sonido.");
+        }
+    }
+
+    //metodo para crear la grilla(ladrillos).
     public final void grilla() {
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 5; j++) {
@@ -55,15 +97,18 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         }
     }
 
+    //metodo para iniciar el juego.
     public void start() {
         juego.resume();
         pausa.set(false);
     }
 
+    //metodo para pausar el juego.
     public final void stop() {
         juego.suspend();
     }
 
+    //metodo para que el hilo deje de correr.
     public void destroy() {
         juego.resume();
         pausa.set(false);
@@ -71,6 +116,7 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         pausa.set(true);
     }
 
+    //metodo para inciar el hilo de la interfaz runnable.
     @Override
     public void run() {
         while (true) {
@@ -88,6 +134,7 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         }
     }
 
+    //metodo para comprobar cuando se han roto todos los ladrillos y si es asi, cambia de nivel.
     public void ganar() {
         if (ladriTotal == 0) {
             if (nivel == 3) {
@@ -110,6 +157,7 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         }
     }
 
+    //metodo que pinta todos los componentes del juego.
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -118,7 +166,7 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         bola.paint(g);
         if (pausa.get() == true) {
             g.setColor(Color.WHITE);
-            g.setFont(fuente.fuente());
+            g.setFont(fuente());
             g.drawString("Presione espacio para Empezar", 65, 240);
         }
         for (int i = 0; i < 7; i++) {
@@ -128,6 +176,7 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         }
     }
 
+    //este metodo dice si el jugador ya perdio, si no es asi devuelve la bola y la barra a su estado inicial.
     private void reStart() {
         if (vidas != 0) {
             bola.setX(BOLA_POS_INICIALX);
@@ -144,6 +193,7 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         }
     }
 
+    //metodo para comparar cuando rebota en una pared o cuando pierde.
     public void rebotePared() {
         if (bola.getX() < 0 || bola.getX() > 670) {
             bola.setDirX(bola.getDirX() * -1);
@@ -166,12 +216,15 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         }
     }
 
+    //metodo para comparar cuando rebota contra la barra.
     public void reboteBarra() {
         if (inmunidad == false) {
             if (barra.golpeArriba(bola.getX(), bola.getY())) {
+                ReproducirSonido(1);
                 bola.setDirY(bola.getDirY() * -1);
                 inmunidad = true;
             } else if (barra.golpeIzquierda(bola.getX(), bola.getY()) || barra.golpeDerecha(bola.getX(), bola.getY())) {
+                ReproducirSonido(1);
                 bola.setDirY(bola.getDirY() * -1);
                 bola.setDirX(bola.getDirX() * -1);
                 inmunidad = true;
@@ -179,48 +232,57 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         }
     }
 
+    //metodo para comprobar cuando rebota contra la grilla(ladrillos).
     public void reboteGrilla() {
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 5; j++) {
                 if (grilla[i][j].golpeArriba(bola.getX() + 10, bola.getY() + 10)) {
+                    ReproducirSonido(2);
                     bola.setDirY(bola.getDirY() * -1);
                     inmunidad = false;
                     break;
                 }
                 if (grilla[i][j].golpeAbajo(bola.getX() + 10, bola.getY() + 10)) {
+                    ReproducirSonido(2);
                     bola.setDirY(bola.getDirY() * -1);
                     inmunidad = false;
                     break;
                 }
                 if (grilla[i][j].golpeDerecha(bola.getX() + 10, bola.getY() + 10)) {
+                    ReproducirSonido(2);
                     bola.setDirX(bola.getDirX() * -1);
                     inmunidad = false;
                     break;
                 }
                 if (grilla[i][j].golpeIzquierda(bola.getX() + 10, bola.getY() + 10)) {
+                    ReproducirSonido(2);
                     bola.setDirX(bola.getDirX() * -1);
                     inmunidad = false;
                     break;
                 }
                 if (grilla[i][j].golpeEsquinaAD(bola.getX() + 10, bola.getY() + 10)) {
+                    ReproducirSonido(2);
                     bola.setDirX(bola.getDirX() * -1);
                     bola.setDirY(bola.getDirY() * -1);
                     inmunidad = false;
                     break;
                 }
                 if (grilla[i][j].golpeEsquinaAI(bola.getX() + 10, bola.getY() + 10)) {
+                    ReproducirSonido(2);
                     bola.setDirX(bola.getDirX() * -1);
                     bola.setDirY(bola.getDirY() * -1);
                     inmunidad = false;
                     break;
                 }
                 if (grilla[i][j].golpeEsquinaArD(bola.getX() + 10, bola.getY() + 10)) {
+                    ReproducirSonido(2);
                     bola.setDirX(bola.getDirX() * -1);
                     bola.setDirY(bola.getDirY() * -1);
                     inmunidad = false;
                     break;
                 }
                 if (grilla[i][j].golpeEsquinaArI(bola.getX() + 10, bola.getY() + 10)) {
+                    ReproducirSonido(2);
                     bola.setDirX(bola.getDirX() * -1);
                     bola.setDirY(bola.getDirY() * -1);
                     inmunidad = false;
@@ -230,8 +292,24 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         }
     }
 
+    //metodo para cambiar la fuente a una personalizada y si no la encuentra la fuente arial.
+    public Font fuente() {
+        try {
+            InputStream is = getClass().getResourceAsStream("/imagenes/batmfa__.ttf");
+            font = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (FontFormatException | IOException ex) {
+            System.err.println("batmfa__.ttf" + " No se cargo la fuente");
+            font = new Font("Arial", Font.PLAIN, 14);
+        }
+        Font tfont = font.deriveFont(Font.BOLD, 25);
+        return tfont;
+    }
+
+    //clase Escucha que hereda de KeyApapter el metodo keyPressed y implementa la interfaz MouseMotionListener
+    // y sus metodos.
     private class Escucha extends KeyAdapter implements MouseMotionListener {
 
+        //metodo que comprar cuando se presiona espacio para inciar el juego
         @Override
         public void keyPressed(KeyEvent ke) {
             int key = ke.getKeyCode();
@@ -244,6 +322,7 @@ public class Tablero extends JPanel implements Runnable, Constantes {
         public void mouseDragged(MouseEvent e) {
         }
 
+        //metodo para mover la barra y cuando esta pausado el juego, mueve la barra y la bola.
         @Override
         public void mouseMoved(MouseEvent e) {
             if (pausa.get() == false) {
@@ -253,23 +332,6 @@ public class Tablero extends JPanel implements Runnable, Constantes {
                 bola.setX(e.getX() - (bola.width / 2));
                 repaint();
             }
-        }
-    }
-
-    public class Fuente {
-
-        private Font font;
-
-        public Font fuente() {
-            try {
-                InputStream is = getClass().getResourceAsStream("/imagenes/batmfa__.ttf");
-                font = Font.createFont(Font.TRUETYPE_FONT, is);
-            } catch (FontFormatException | IOException ex) {
-                System.err.println("batmfa__.ttf" + " No se cargo la fuente");
-                font = new Font("Arial", Font.PLAIN, 14);
-            }
-            Font tfont = font.deriveFont(Font.BOLD, 25);
-            return tfont;
         }
     }
 }
